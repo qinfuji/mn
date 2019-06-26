bd671219ab1ccf4baede05737b04f612e39d5663
qinfuji <7748112@qq.com>
2019-06-27 01:01:59 +0800

    增加几何计算类

--- src/main/java/com/mn/modules/api/utils/GeometryUtil.java
+++ src/main/java/com/mn/modules/api/utils/GeometryUtil.java
@@ -1,24 +1,199 @@
 package com.mn.modules.api.utils;
 
 import java.awt.geom.Point2D;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * 判断一个点是否在一个多边形内
  * 提供了三种计算方式，孰好孰坏在实践中比较一下
+ *
  * @author xiongshiyan at 2019/1/11 , contact me with email yanshixiong@126.com or phone 15208384257
  */
 public class GeometryUtil {
     private static final int POLYGON_MIN_SIZE = 3;
 
-    public static boolean isPtInPolygon (Point2D.Double point , List<Point2D.Double> polygon) {
+
+    /**
+     * 没有测试
+     * @param pointX
+     * @param pointY
+     * @param MapUnits
+     * @return
+     */
+    public static double calcArea(ArrayList pointX, ArrayList pointY, String MapUnits) {
+
+        int Count = pointX.size();
+        if (Count > 2) {
+            double mtotalArea = 0;
+            if (MapUnits == "DEGREES")//经纬度坐标下的球面多边形
+            {
+                double LowX = 0.0;
+                double LowY = 0.0;
+                double MiddleX = 0.0;
+                double MiddleY = 0.0;
+                double HighX = 0.0;
+                double HighY = 0.0;
+
+                double AM = 0.0;
+                double BM = 0.0;
+                double CM = 0.0;
+
+                double AL = 0.0;
+                double BL = 0.0;
+                double CL = 0.0;
+
+                double AH = 0.0;
+                double BH = 0.0;
+                double CH = 0.0;
+
+                double CoefficientL = 0.0;
+                double CoefficientH = 0.0;
+
+                double ALtangent = 0.0;
+                double BLtangent = 0.0;
+                double CLtangent = 0.0;
+
+                double AHtangent = 0.0;
+                double BHtangent = 0.0;
+                double CHtangent = 0.0;
+
+                double ANormalLine = 0.0;
+                double BNormalLine = 0.0;
+                double CNormalLine = 0.0;
+
+                double OrientationValue = 0.0;
+
+                double AngleCos = 0.0;
+
+                double Sum1 = 0.0;
+                double Sum2 = 0.0;
+                double Count2 = 0;
+                double Count1 = 0;
+
+
+                double Sum = 0.0;
+                double Radius = 6378000;
+
+                for (int i = 0; i < Count; i++) {
+                    if (i == 0) {
+                        LowX = (double) pointX.get(Count - 1) * Math.PI / 180;
+                        LowY = (double) pointY.get(Count - 1) * Math.PI / 180;
+                        MiddleX = (double) pointX.get(0) * Math.PI / 180;
+                        MiddleY = (double) pointY.get(0) * Math.PI / 180;
+                        HighX = (double) pointX.get(1) * Math.PI / 180;
+                        HighY = (double) pointY.get(1) * Math.PI / 180;
+                    } else if (i == Count - 1) {
+                        LowX = (double) pointX.get(Count - 2) * Math.PI / 180;
+                        LowY = (double) pointY.get(Count - 2) * Math.PI / 180;
+                        MiddleX = (double) pointX.get(Count - 1) * Math.PI / 180;
+                        MiddleY = (double) pointY.get(Count - 1) * Math.PI / 180;
+                        HighX = (double) pointX.get(0) * Math.PI / 180;
+                        HighY = (double) pointY.get(0) * Math.PI / 180;
+                    } else {
+                        LowX = (double) pointX.get(i - 1) * Math.PI / 180;
+                        LowY = (double) pointY.get(i - 1) * Math.PI / 180;
+                        MiddleX = (double) pointX.get(i) * Math.PI / 180;
+                        MiddleY = (double) pointY.get(i) * Math.PI / 180;
+                        HighX = (double) pointX.get(i + 1) * Math.PI / 180;
+                        HighY = (double) pointY.get(i + 1) * Math.PI / 180;
+                    }
+
+                    AM = Math.cos(MiddleY) * Math.cos(MiddleX);
+                    BM = Math.cos(MiddleY) * Math.sin(MiddleX);
+                    CM = Math.sin(MiddleY);
+                    AL = Math.cos(LowY) * Math.cos(LowX);
+                    BL = Math.cos(LowY) * Math.sin(LowX);
+                    CL = Math.sin(LowY);
+                    AH = Math.cos(HighY) * Math.cos(HighX);
+                    BH = Math.cos(HighY) * Math.sin(HighX);
+                    CH = Math.sin(HighY);
+
+
+                    CoefficientL = (AM * AM + BM * BM + CM * CM) / (AM * AL + BM * BL + CM * CL);
+                    CoefficientH = (AM * AM + BM * BM + CM * CM) / (AM * AH + BM * BH + CM * CH);
+
+                    ALtangent = CoefficientL * AL - AM;
+                    BLtangent = CoefficientL * BL - BM;
+                    CLtangent = CoefficientL * CL - CM;
+                    AHtangent = CoefficientH * AH - AM;
+                    BHtangent = CoefficientH * BH - BM;
+                    CHtangent = CoefficientH * CH - CM;
+
+
+                    AngleCos = (AHtangent * ALtangent + BHtangent * BLtangent + CHtangent * CLtangent) / (Math.sqrt(AHtangent * AHtangent + BHtangent * BHtangent + CHtangent * CHtangent) * Math.sqrt(ALtangent * ALtangent + BLtangent * BLtangent + CLtangent * CLtangent));
+
+                    AngleCos = Math.acos(AngleCos);
+
+                    ANormalLine = BHtangent * CLtangent - CHtangent * BLtangent;
+                    BNormalLine = 0 - (AHtangent * CLtangent - CHtangent * ALtangent);
+                    CNormalLine = AHtangent * BLtangent - BHtangent * ALtangent;
+
+                    if (AM != 0) {
+                        OrientationValue = ANormalLine / AM;
+                    } else if (BM != 0) {
+                        OrientationValue = BNormalLine / BM;
+                    } else {
+                        OrientationValue = CNormalLine / CM;
+                    }
+
+
+                    if (OrientationValue > 0) {
+                        Sum1 += AngleCos;
+                        Count1++;
+
+                    } else {
+                        Sum2 += AngleCos;
+                        Count2++;
+                        //Sum +=2*Math.PI-AngleCos;
+                    }
+
+                }
+
+                if (Sum1 > Sum2) {
+                    Sum = Sum1 + (2 * Math.PI * Count2 - Sum2);
+                } else {
+                    Sum = (2 * Math.PI * Count1 - Sum1) + Sum2;
+                }
+
+                //平方米
+                mtotalArea = (Sum - (Count - 2) * Math.PI) * Radius * Radius;
+            } else { //非经纬度坐标下的平面多边形
+
+                int i, j;
+                //double j;
+                double p1x, p1y;
+                double p2x, p2y;
+                for (i = Count - 1, j = 0; j < Count; i = j, j++) {
+
+                    p1x = (double) pointX.get(i);
+                    p1y = (double) pointY.get(i);
+
+                    p2x = (double) pointX.get(j);
+                    p2y = (double) pointY.get(j);
+
+                    mtotalArea += p1x * p2y - p2x * p1y;
+                }
+                mtotalArea /= 2.0;
+            }
+            return mtotalArea;
+        }
+        return 0;
+    }
+
+    /**
+     * @param point
+     * @param polygon 多边形的顶点
+     * @return
+     */
+    public static boolean isPtInPolygon(Point2D.Double point, List<Point2D.Double> polygon) {
         assertParams(point, polygon);
 
-        int iSum,iIndex;
-        double dLon1 , dLon2 , dLat1 , dLat2 , dLon;
+        int iSum, iIndex;
+        double dLon1, dLon2, dLat1, dLat2, dLon;
         int size = polygon.size();
         iSum = 0;
-        for (iIndex = 0; iIndex<size; iIndex++) {
+        for (iIndex = 0; iIndex < size; iIndex++) {
             if (iIndex == size - 1) {
                 dLon1 = polygon.get(iIndex).getX();
                 dLat1 = polygon.get(iIndex).getY();
@@ -35,7 +210,7 @@ public class GeometryUtil {
                     || ((point.y >= dLat2) && (point.y < dLat1))) {
                 if (Math.abs(dLat1 - dLat2) > 0) {
                     //得到 A点向左射线与边的交点的x坐标：
-                    dLon = dLon1 - ((dLon1 - dLon2) * (dLat1 - point.y) ) / (dLat1 - dLat2);
+                    dLon = dLon1 - ((dLon1 - dLon2) * (dLat1 - point.y)) / (dLat1 - dLat2);
                     // 如果交点在A点左侧（说明是做射线与 边的交点），则射线与边的全部交点数加一：
                     if (dLon < point.x) {
                         iSum++;
@@ -47,7 +222,9 @@ public class GeometryUtil {
     }
 
     /**
-     * 返回一个点是否在一个多边形区域内， 如果点位于多边形的顶点或边上，不算做点在多边形内，返回false
+     * @param point   检测点
+     * @param polygon 多边形的顶点
+     *                返回一个点是否在一个多边形区域内， 如果点位于多边形的顶点或边上，不算做点在多边形内，返回false
      */
     public static boolean isPointInPoly(Point2D.Double point, List<Point2D.Double> polygon) {
         assertParams(point, polygon);
@@ -67,11 +244,12 @@ public class GeometryUtil {
 
     /**
      * 判断点是否在多边形内，如果点位于多边形的顶点或边上，也算做点在多边形内，直接返回true
-     * @param point 检测点
-     * @param polygon   多边形的顶点
-     * @return      点在多边形内返回true,否则返回false
+     *
+     * @param point   检测点
+     * @param polygon 多边形的顶点
+     * @return 点在多边形内返回true, 否则返回false
      */
-    public static boolean isPtInPoly(Point2D.Double point, List<Point2D.Double> polygon){
+    public static boolean isPtInPoly(Point2D.Double point, List<Point2D.Double> polygon) {
         assertParams(point, polygon);
 
         int N = polygon.size();
@@ -89,8 +267,8 @@ public class GeometryUtil {
         //left vertex
         p1 = polygon.get(0);
         //check all rays
-        for(int i = 1; i <= N; ++i){
-            if(p.equals(p1)){
+        for (int i = 1; i <= N; ++i) {
+            if (p.equals(p1)) {
                 //p is an vertex
                 return boundOrVertex;
             }
@@ -98,55 +276,55 @@ public class GeometryUtil {
             //right vertex
             p2 = polygon.get(i % N);
             //ray is outside of our interests
-            if(p.x < Math.min(p1.x, p2.x) || p.x > Math.max(p1.x, p2.x)){
+            if (p.x < Math.min(p1.x, p2.x) || p.x > Math.max(p1.x, p2.x)) {
                 p1 = p2;
                 //next ray left point
                 continue;
             }
 
             //ray is crossing over by the algorithm (common part of)
-            if(p.x > Math.min(p1.x, p2.x) && p.x < Math.max(p1.x, p2.x)){
+            if (p.x > Math.min(p1.x, p2.x) && p.x < Math.max(p1.x, p2.x)) {
                 //x is before of ray
-                if(p.y <= Math.max(p1.y, p2.y)){
+                if (p.y <= Math.max(p1.y, p2.y)) {
                     //overlies on a horizontal ray
-                    if(p1.x == p2.x && p.y >= Math.min(p1.y, p2.y)){
+                    if (p1.x == p2.x && p.y >= Math.min(p1.y, p2.y)) {
                         return boundOrVertex;
                     }
 
                     //ray is vertical
-                    if(p1.y == p2.y){
+                    if (p1.y == p2.y) {
                         //overlies on a vertical ray
-                        if(p1.y == p.y){
+                        if (p1.y == p.y) {
                             return boundOrVertex;
                             //before ray
-                        }else{
+                        } else {
                             ++intersectCount;
                         }
                         //cross point on the left side
-                    }else{
+                    } else {
                         //cross point of y
                         double xinters = (p.x - p1.x) * (p2.y - p1.y) / (p2.x - p1.x) + p1.y;
                         //overlies on a ray
-                        if(Math.abs(p.y - xinters) < precision){
+                        if (Math.abs(p.y - xinters) < precision) {
                             return boundOrVertex;
                         }
 
                         //before ray
-                        if(p.y < xinters){
+                        if (p.y < xinters) {
                             ++intersectCount;
                         }
                     }
                 }
                 //special case when ray is crossing through the vertex
-            }else{
+            } else {
                 //p crossing over p2
-                if(p.x == p2.x && p.y <= p2.y){
+                if (p.x == p2.x && p.y <= p2.y) {
                     //next vertex
-                    Point2D.Double p3 = polygon.get((i+1) % N);
+                    Point2D.Double p3 = polygon.get((i + 1) % N);
                     //p.x lies between p1.x & p3.x
-                    if(p.x >= Math.min(p1.x, p3.x) && p.x <= Math.max(p1.x, p3.x)){
+                    if (p.x >= Math.min(p1.x, p3.x) && p.x <= Math.max(p1.x, p3.x)) {
                         ++intersectCount;
-                    }else{
+                    } else {
                         intersectCount += 2;
                     }
                 }
@@ -156,15 +334,16 @@ public class GeometryUtil {
         }
 
         //偶数在多边形外
-        if(intersectCount % 2 == 0){
+        if (intersectCount % 2 == 0) {
             return false;
             //奇数在多边形内
         } else {
             return true;
         }
     }
+
     private static void assertParams(Point2D.Double point, List<Point2D.Double> polygon) {
-        if(null == point || null == polygon || polygon.size() < POLYGON_MIN_SIZE) {
+        if (null == point || null == polygon || polygon.size() < POLYGON_MIN_SIZE) {
             throw new IllegalArgumentException("参数不能为空，且多边形点数大于3");
         }
     }
---
+++ src/main/java/com/mn/modules/api/utils/MapHelper.java
@@ -0,0 +1,92 @@
+package com.mn.modules.api.utils;
+
+import java.awt.geom.Point2D;
+import java.util.List;
+
+public class MapHelper {
+
+    /**
+     * 地球半径,单位：千米
+     */
+    private static double EarthRadius = 6378.137;
+
+    /**
+     * 经纬度转化成弧度
+     * @param d 经度/纬度
+     * @return
+     */
+    private static double rad(double d) {
+        return d * Math.PI / 180.0;
+    }
+
+    /**
+     * 计算两个坐标点之间的距离
+     *
+     * @param firstLatitude   第一个坐标的纬度
+     * @param firstLongitude  第一个坐标的经度
+     * @param secondLatitude  第二个坐标的纬度
+     * @param secondLongitude 第二个坐标的经度
+     * @return 返回两点之间的距离，单位：公里/千米
+     */
+    public static double getDistance(double firstLatitude, double firstLongitude,
+                                     double secondLatitude, double secondLongitude) {
+        double firstRadLat = rad(firstLatitude);
+        double firstRadLng = rad(firstLongitude);
+        double secondRadLat = rad(secondLatitude);
+        double secondRadLng = rad(secondLongitude);
+
+        double a = firstRadLat - secondRadLat;
+        double b = firstRadLng - secondRadLng;
+        double cal = 2 * Math.asin(Math.sqrt(Math.pow(Math.sin(a / 2), 2) + Math.cos(firstRadLat)
+                * Math.cos(secondRadLat) * Math.pow(Math.sin(b / 2), 2))) * EarthRadius;
+        double result = Math.round(cal * 10000d) / 10000d;
+        // result = result * 1000;  //单位米
+        return result;
+    }
+
+    /**
+     * 计算两个坐标点之间的距离
+     *
+     * @param firstPoint  第一个坐标点的（纬度,经度） 例如："31.2553210000,121.4620020000"
+     * @param secondPoint 第二个坐标点的（纬度,经度） 例如："31.2005470000,121.3269970000"
+     * @return 返回两点之间的距离，单位：公里/千米
+     */
+    public static double GetPointDistance(String firstPoint, String secondPoint) {
+        String[] firstArray = firstPoint.split(",");
+        String[] secondArray = secondPoint.split(",");
+        double firstLatitude = Double.valueOf(firstArray[0].trim());
+        double firstLongitude = Double.valueOf(firstArray[1].trim());
+        double secondLatitude = Double.valueOf(secondArray[0].trim());
+        double secondLongitude = Double.valueOf(secondArray[1].trim());
+        return getDistance(firstLatitude, firstLongitude, secondLatitude, secondLongitude);
+    }
+
+    /**
+     *
+     * 这是利用:纬度间距1度=111.1km 的关系计算的；经度1度=111.1cosA (A为计算经度之处的纬度，比如在北纬60度处计算经度1度的距离，就是111.1Xcos60=111.1/2km)
+     *
+     * @param polygons
+     * @return 平方千米
+     */
+    private static double CalculatePolygonArea(List<Point2D.Double> polygons)
+    {
+        double area = 0;
+        if (polygons.size() > 2)
+        {
+            for (int i = 0; i < polygons.size() - 1; i++)
+            {
+                Point2D.Double p1 = polygons.get(i);
+                Point2D.Double p2 = polygons.get(i+1);
+                //x:经度 y:维度
+                //return (p1.first-p0.first)*(p2.second-p0.second)-(p1.second-p0.second)*(p2.first-p0.first);
+                area += rad(p2.getX() - p1.getX()) * (2 + Math.sin(rad(p1.getY())) + Math.sin(rad(p2.getY())));
+            }
+            area = area * EarthRadius * EarthRadius  / 2.0;
+        }
+        return Math.abs(area);
+    }
+
+
+
+
+}
